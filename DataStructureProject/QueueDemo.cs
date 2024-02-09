549648232f5e3bbb8a6e6b32c8d80fdfaadde435
Lokeshgithubcodes <ramisettylokesh642@gmail.com>
2024-02-09 23:21:20 +0530

    first commit

---
+++ DataStructureProject/QueueDemo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructureProject
+{
+    public class QueueDemo
+    {
+        static void Main(string[] args)
+        {
+            Queue<int> q= new Queue<int>();
+
+            q.Enqueue(1);
+            q.Enqueue(2);
+            q.Enqueue(3);
+            q.Enqueue(4);
+
+            while(q.Count > 0)
+            {
+                int e= q.Dequeue();
+                Console.WriteLine("Dequeue elements are: "+e);
+            }
+        }
+    }
+}
---
+++ DataStructureProject/StackDemo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructureProject
+{
+    public class StackDemo
+    {
+        static void Main(string[] args)
+        {
+            Stack<int> s= new Stack<int>();
+            s.Push(1);
+            s.Push(2);
+            s.Push(3);
+            s.Push(4);
+
+            while (s.Count > 0)
+            {
+                int e=s.Pop();
+                Console.WriteLine("poped elements are:"+e);
+            }
+        }
+    }
+}
